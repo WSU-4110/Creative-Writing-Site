612f31e63d5f2a31e15f8982cfc12307bc3ab71b
brinurmi <ev5443@wayne.edu>
2020-03-23 21:01:20 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@
 //(2)check to make sure username doesnt already exist
 //(3)check if email is already in the database. email must be unique
 //(4)validate email address by sending an email to new user
-//**(5) fix errors with "POST". it might be syntax from bootstrap. insetad, try: $username = filter_input(INPUT_POST,'username);
+
 
 //get values from account creation page
 $username=$_POST['username'];
@@ -57,70 +57,6 @@ else{
     echo "All entries are required!";
     die();
 }
-
-
-
-
-/*
-if(!empty($username)){
-    if(!empty($firstname)){
-        if(!empty($lastname)){
-            if(!empty($email)){
-                if(!empty($password)){ //if all entries are filled,
-                    $host = "localhost";
-                    $dbuser="root";
-                    $dbpass="";
-                    $dbname="users";
-                    
-                    //create connection
-                    $conn= new mysqli($host,$dbuser,$dbpass.$dbname);
-                    
-                    //check for successful connection
-                    //connection failed
-                    if(mysqli_connect_error()){
-                        die('Connect Error ('.mysqli_connect_errno() .')'.mysqli_connect_error());
-                    }
-                    else{//connection successful
-                        $sql="INSERT INTO users(username,firstname,lastname,email,password) values('$username','$firstname','$lastname','$email','$password')";
-                        if($conn->query($sql)){//successfuly inserted
-                            echo "Sign-up was successful!Welcome to Wordly";
-                        }
-                        else{//insertion error
-                            echo "Error: ". $sql ."<br>". $conn->error;
-                            
-                        }
-                        //close connection from databse
-                        $conn->close();
-                    }
-                }
-                else{//password entry error
-                    echo "Password should not be left empty.";
-                    die();
-                    
-                }
-            }
-            else{//email entry error
-                echo "Email should not be left empty.";
-                die();
-            }
-        }
-        else{//lastname entry error
-            echo "Last name should not be left empty.";
-            die();    
-        }
-        
-    }
-    else{//first name entry error
-        echo "First name should not be left empty.";
-        die();
-    }
-}
-else{//username entry error
-    echo "username should not be left empty.";
-    die();
-    
-}
-*/
 
 
 ?>
