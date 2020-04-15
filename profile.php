a66e43847972fffb6d46b5acab6a0117d82c11ac
Nidal Abihana <gl6180@wayne.edu>
2020-04-14 20:36:30 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -9,6 +9,8 @@ header("location: account_creation.php");
 <html lang="en">
     <head>
 	<script src="lorem.js"></script>
+        <script src ="file.js"></script>
+        <link rel="stylesheet" href="upload.css">
         <link rel="stylesheet" href="main.css">
         <title>Writely</title>
         <meta charset="utf-8">
@@ -34,43 +36,169 @@ header("location: account_creation.php");
             </form> 
         </header>
         <script type="text/javascript" src="mobile.js"></script>
-	<main>
 	   <!--<h1>Jerry Garcia</h1>-->
 		 <div class="side_bar">
-	   		<img src="JerryGarcia-DEMOProfilePic.jpg" width="213" height="282">
-			 <p><Strong>Jerry Garcia </strong> &nbsp
-			 <button onclick="window.location.href= 'Profile.html';">Follow</button></p>
-			 <a href="profile_BioPage.html">Bio</a> <a href="https://jerrygarcia.com/jerrys-story/">My Website</a>
-	  		 <p><b>User Rating: </b><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
-			 <p><b>Followers: </b><i>123,420</i></p>
-			 <p><i>active since 08/01/1942</i></p>
-	  	</div>
-		<!--<h2>Featured Posts</h2>-->
-	 <!-- <div id="propage_center">-->
-		<div class="post">
-		 <b>Corgi Haiku</b><br>
-			<p>When I'm on the beach<br>My paws are always full of sand<br>Salt water makes me gag</p>	
-		</div>
-		<div class="post">
-		<b>Journal Entry #1</b><br>
-			<p><i>Feb. 01, 2020: </i>The waves are perfect today! I had to call off work at Trader Joe's<br> today because my surf board needs waxing.</p>
-		</div>
-		<div class="post">		
-		<b>Clarity Pyramid: Sunshine</b><br>
-			<p>rays<br>93*F<br>beaming<br>hot sand<br>warm breeze<br>
-			bright golden light<br>making tacos in my yurt</p>
-		</div>
-		 <div class="post">
-			<img img src="JerryGarcia-DEMOProfilePic.jpg" alt="profile" height= 20px; width=20px;><b> Jerry Garcia:</b>
-           		<div data-lorem="2-4p"></div>
-        	</div>
-		<div class="post">
-			<img img src="JerryGarcia-DEMOProfilePic.jpg" alt="profile" height= 20px; width=20px;><b> Jerry Garcia:</b>
-            		<div data-lorem="2-4p"></div>
-        	</div>
-	  <!--</div>-->
-	</main>
+             <p><Strong><?php echo $login_session; ?></Strong></p>
+                 <main class="uploadmain">
+                
+                    <button id='headerbtn'>Add bio</button> 
             
+                    
+                     
+                     
+        <div id="editor" contenteditable="true" data-text="Enter text here..." required></div>
+                <!--scripts to handle the text formatting buttons-->
+                <script>
+                    $(document).ready(function(){
+                        $("#editor").hide();
+                        $(".uploadtable").hide();
+                        $("#submitbtn").hide();
+                        $("#fileupload").hide();
+
+                        $('#headerbtn').on('click', function(){
+                            $(".uploadtable").fadeToggle(100);
+                            $("#editor").fadeToggle(100);                            
+                            $("#submitbtn").fadeToggle(100);
+                            $("#uploadbtn").fadeToggle(100);
+                            $(".uploadwrapper, .uploadwrapper2").toggleClass("uploadwrapper uploadwrapper2");
+                        })
+                        
+
+                        $("#uploadbtn").on('click', function(){
+                            $("#fileupload").fadeToggle(300);
+                            $("#headerbtn").fadeToggle(300);
+                            $("#submitbtn").fadeToggle(300);
+                        })
+
+
+                        //buttons for text editor
+                        $('#boldbtn').on('click', function(){
+                            $('#editor').focus();
+                            document.execCommand('bold');
+                            $(this).toggleClass("gray");
+                        });
+
+                        $('#italicbtn').on('click', function(){
+                            $('#editor').focus();
+                            document.execCommand('italic');                            
+                            $(this).toggleClass("gray");
+                        });
+
+                        $('#underbtn').on('click', function(){
+                            $('#editor').focus();
+                            document.execCommand('underline');                            
+                            $(this).toggleClass("gray");
+                        });
+
+                        $("#fs").change(function(){
+                            $('#editor').css("font-family", $(this).val());
+                            $('#editor').focus();
+                        });
+
+                        $("#size").change(function(){
+                            $('#editor').css("font-size", $(this).val() + "px");
+                            $('#editor').focus();
+                        });
+
+                        $("#submitbtn").click(function(){
+                            var writing = $("#editor").html();
+                            sessionStorage.setItem("writing", writing);
+
+                            $(document).ready(function(){
+                                $("#content").val(writing);
+                            });
+                            
+                            if(writing == ""){
+                                alert("Text box cannot be blank");
+                                return false;
+                            }
+
+                        });
+                    });
+                    
+                    
+                    
+                </script>
+
+
+
+
+
+
+            </main>   
+			 
+	  	</div>
+		
+        
+		<div class="post">
+		 <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "upload";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT id, content, Title, Publicity FROM post";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                
+                while($row = $result->fetch_assoc()) {
+                    if($row["Publicity"] != 0){
+                        echo "<div class='post'><img img src='Generic-Profile.png' alt='profile' height= 20px; width=20px;><b> John Doe:</b><br><br><i>". $row["Title"]. ":</i> <br>". $row["content"]. "</div>" ;
+                    }
+                    
+                }
+            } else {
+                echo "0 results";
+            }
+
+            $conn->close();
+        ?>
+        	</div>
+        
+	  <div style="text-align: center;">
+            <?php
+                $id = isset($_GET['content']) ? $_GET['content'] : '';
+                
+                $host = "localhost";
+                $dbUsername = "root";
+                $dbPassword = "";
+                $dbname = "bio";
+                //create connection
+                $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+                
+                if (mysqli_connect_error()) {
+                    die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+                } 
+
+                else {
+                $insert = "INSERT Into bio (content) VALUES ('$content')";
+                //Prepare statement
+                
+                $stmt = $conn->prepare($insert);
+                $stmt->bind_param("s", $content);
+                $stmt->execute();
+                    
+                echo "New record created successfully";
+                
+                
+                
+                }
+
+
+                die();
+
+            ?>
+        </div>
+
             <div class="end_footer">
                 Contact us!<br>
                 Send us an <a href= "mailto:writely@gmail.com">e-mail!</a><br>
